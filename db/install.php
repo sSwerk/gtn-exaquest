e3fc1c9e24782dfdbd2883a9b8bc40e03e2d8462
fabiopernegger <fabio.pernegger@gmail.com>
2022-04-14 15:09:30 +0200

    Added structure in install.php to create custom fields and categories for questions

--- db/install.php
+++ db/install.php
@@ -19,7 +19,38 @@
 
 require_once __DIR__ . '/../inc.php';
 
+use \core_customfield\field_controller;
+
 // called when installing a plugin
 function xmldb_block_exaquest_install() {
-    
+    $handler = qbank_customfields\customfield\question_handler::create();
+    $c1id = $handler->create_category();
+    $c1 = $handler->get_categories_with_fields()[$c1id];
+    $handler->rename_category($c1, 'JKUfields2');
+    $record = new stdClass();
+    $record->name = 'JKUField';
+    $record->shortname = "JKU";
+    $record->type = 'checkbox';
+    $record->sortorder = 0;
+    $configdata = [];
+    $configdata += [
+        'required' => 0,
+        'uniquevalues' => 0,
+        'locked' => 0,
+        'visibility' => 2,
+        'defaultvalue' => '',
+        'displaysize' => 0,
+        'maxlength' => 0,
+        'ispassword' => 0,
+        'link' => '',
+        'linktarget' => '',
+        'checkbydefault' => 0,
+        'startyear' => 2000,
+        'endyear' => 3000,
+        'includetime' => 1,
+    ];
+    $record->configdata = json_encode($configdata);
+    $field = field_controller::create(0, (object)['type' => $record->type], $c1);
+    $handler->save_field_configuration($field, $record);
+
 }
