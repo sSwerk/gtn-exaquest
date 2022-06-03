60d41584a7b649dca6d13970cb14b90934233606
fabiopernegger <fabio.pernegger@gmail.com>
2022-06-03 11:49:44 +0200

    new question status table added

---
+++ db/upgrade.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../inc.php';
+
+function xmldb_block_exaquest_upgrade($oldversion)
+{
+    global $DB, $CFG;
+    $dbman = $DB->get_manager();
+    $return_result = true;
+
+    if ($oldversion < 2022060300) {
+
+        $table = new xmldb_table('block_exaquestquestionstatus');
+
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('questionid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('status', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Exaquest savepoint reached.
+        upgrade_block_savepoint(true, 2022060300, 'exaquest');
+    }
+}
