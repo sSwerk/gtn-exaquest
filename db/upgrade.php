15be8f2d3b95654577854273fd2694ff4df58016
Stefan <stefan.swerk@jku.at>
2022-07-01 19:41:36 +0200

    Include similarity_comparison plugin and prepare DB table.

--- db/upgrade.php
+++ db/upgrade.php
@@ -21,10 +21,11 @@ function xmldb_block_exaquest_upgrade($oldversion)
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
-
+        
         // Exaquest savepoint reached.
         upgrade_block_savepoint(true, 2022060300, 'exaquest');
     }
+    
     if ($oldversion < 2022060902) {
         // Creating roles and assigning capabilities
         // Done as a task AFTER the installation/upgrade, because the capabilities only exist at the end/after the installation/upgrade.
@@ -35,5 +36,35 @@ function xmldb_block_exaquest_upgrade($oldversion)
         // Exaquest savepoint reached.
         upgrade_block_savepoint(true, 2022060902, 'exaquest');
     }
+    
+    if ($oldversion < 2022062400) {
+        // Define table block_exaquest_similarity to be created.
+        $table = new xmldb_table('block_exaquest_similarity');
+
+        // Adding fields to table block_exaquest_similarity.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('question_id1', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('question_id2', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('is_similar', XMLDB_TYPE_INTEGER, '1', null, null, null, null);
+        $table->add_field('similarity', XMLDB_TYPE_NUMBER, '20, 19', null, null, null, null);
+        $table->add_field('timestamp_calculation', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('threshold', XMLDB_TYPE_NUMBER, '20, 19', null, null, null, null);
+        $table->add_field('algorithm', XMLDB_TYPE_CHAR, '20', null, null, null, null);
+
+        // Adding keys to table block_exaquest_similarity.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('fk_questionid_1', XMLDB_KEY_FOREIGN, ['question_id1'], 'question', ['id']);
+        $table->add_key('fk_questionid_2', XMLDB_KEY_FOREIGN, ['question_id2'], 'question', ['id']);
+
+        // Conditionally launch create table for block_exaquest_similarity.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Exaquest savepoint reached
+        upgrade_block_savepoint(true, 2022062400, 'exaquest');
+    }
+    
     return $return_result;
 }
+
---
+++ similarity_comparison.php
@@ -0,0 +1,86 @@
+<?php
+require __DIR__ . '/inc.php';
+
+global $DB, $CFG, $COURSE, $PAGE, $OUTPUT, $USER;
+
+require_once($CFG->dirroot . '/question/editlib.php');
+require_once(__DIR__ . '/questionbank_extensions/exaquest_view.php');
+
+require_once(__DIR__ . '/similarity_comparison/vendor/autoload.php'); // load similarity comparison library
+
+use GTN\import\XlsImporter;
+$testMessage = XlsImporter::test(); // quick and dirty test whether autoload was successful
+\core\notification::add($testMessage, \core\output\notification::NOTIFY_SUCCESS);
+
+$courseID = required_param('courseid', PARAM_INT);
+require_login($courseID);
+
+[$thispageurl, $contexts, $cmid, $cm, $module, $pagevars] = question_edit_setup('questions', '/question/edit.php');
+
+//$course = $DB->get_record('course', array('id' => $courseid));
+$courseContext = context_course::instance($courseID);
+
+$pageParams = array('courseid' => $courseID);
+
+$url = new moodle_url('/blocks/exaquest/similarity_comparison.php', $pageParams);
+$PAGE->set_url($url);
+$PAGE->set_heading(get_string('exaquest:similarity_title', 'block_exaquest'));
+$PAGE->set_title(get_string('exaquest:similarity_title', 'block_exaquest'));
+
+block_exaquest_init_js_css();
+
+$output = $PAGE->get_renderer('block_exaquest');
+
+echo $output->header($courseContext, $courseID, get_string('exaquest:similarity_title', 'block_exaquest'));
+
+$action = optional_param('action', "", PARAM_ALPHAEXT);
+if ($action == 'request_questions') {
+    // get all the users with role "fragesteller" and send them a notification
+    $allfragenersteller = block_exaquest_get_fragenersteller_by_courseid($courseID);
+    $selectedfragenersteller = $_POST["selectedusers"];
+    $frageneersteller = array_intersect_key($allfragenersteller, $selectedfragenersteller);
+    foreach ($frageneersteller as $ersteller) {
+        $messageobject = new stdClass();
+        $messageobject->fullname = $COURSE->fullname;
+        $messageobject->url = new moodle_url('/blocks/exaquest/dashboard.php', ['courseid' => $COURSE->id]);
+        $messageobject->url = $messageobject->url->raw_out(false);
+        $message = get_string('please_create_new_questions', 'block_exaquest', $messageobject);
+        $message = get_string('please_create_new_questions_subject', 'block_exaquest', $messageobject);
+        block_exaquest_send_moodle_notification("newquestionsrequest", $USER->id, $ersteller->id, $message, $message,
+            "Frageerstellung");
+    }
+
+}
+
+echo '<div id="exaquest">';
+if (is_enrolled($courseContext, $USER, "block/exaquest:createquestion")) {
+    // ADD QUESTION
+    echo '<div class="dashboardcard">';
+    $questionbank = new core_question\local\bank\exaquest_view($contexts, $url, $COURSE, $cm);
+    $categoryandcontext = $pagevars["cat"];
+    list($categoryid, $contextid) = explode(',', $categoryandcontext);
+    $catcontext = \context::instance_by_id($contextid);
+    $category = $questionbank->get_current_category_dashboard($categoryandcontext);
+    $canadd = has_capability('moodle/question:add', $catcontext);
+    $questionbank->create_new_question_form_dashboard($category, $canadd);
+    echo '</div>';
+}
+if (is_enrolled($courseContext, $USER, "block/exaquest:modulverantwortlicher")) {
+    if (!isset($frageneersteller) || empty($data->fragenersteller)) {
+        $frageneersteller = block_exaquest_get_fragenersteller_by_courseid($courseID);
+    }
+    $dashboardcard = new \block_exaquest\output\dashboardcard_request_questions($frageneersteller);
+    echo $output->render($dashboardcard);
+}
+if (is_enrolled($courseContext, $USER, "block/exaquest:fragenersteller")) {
+        // QUESTIONS TO REVIEW
+    $questions = block_exaquest_get_questions_to_revise($courseID, $USER->id);
+    if (!isset($questions)) {
+        $questions = [];
+    }
+    $dashboardcard = new \block_exaquest\output\dashboardcard_revise_questions($questions);
+    echo $output->render($dashboardcard);
+}
+
+echo '</div>';
+echo $output->footer(); 
