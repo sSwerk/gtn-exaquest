d4e30e603a8226fe0cefc018ac193434c35b2dc2
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2022-07-12 15:30:57 +0200

    Revert "version"
    This reverts commit 19c39dd0cf34402a0de94fab23fd1c389020866a.

--- classes/output/dashboard.php
+++ classes/output/dashboard.php
@@ -42,7 +42,7 @@ class dashboard implements renderable, templatable {
         $data->questions_count = block_exaquest_get_questionbankentries_by_courseid_count($this->courseid);
         $data->questions_reviewed_count = block_exaquest_get_reviewed_questionbankentries_count($this->courseid);
         $data->questions_to_review_count = block_exaquest_get_questionbankentries_to_be_reviewed_count($this->courseid);
-        $data->questions_released_count = block_exaquest_get_questionbankentries_released_count($this->courseid);
+        $data->questions_released_count = block_exaquest_get_questionbankentries_to_be_reviewed_count($this->courseid);
         $data->questions_released_count = 0;
         $data->questions_released_to_review_count = 0;
 
